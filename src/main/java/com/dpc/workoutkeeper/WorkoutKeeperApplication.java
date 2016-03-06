fd370024a1c55b692582ed302dd91d5f2167529f
Daniel <dan_pc@live.com>
2016-03-06 17:38:01 -0500

    Added project

---
+++ src/main/java/com/dpc/workoutkeeper/WorkoutKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.dpc.workoutkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutKeeperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dpc/workoutkeeper/WorkoutKeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dpc.workoutkeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WorkoutKeeperApplication.class)
+public class WorkoutKeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
